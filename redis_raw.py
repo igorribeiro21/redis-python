53819ec6860833bad7e13c76909846c91ba161e2
igor.ribeiro <igorapalvesribeiro@gmail.com>
2025-10-20 12:27:38 -0300

    chore: add .gitignore and initial redis example script

---
+++ redis_raw.py
@@ -0,0 +1,14 @@
+import redis
+
+redis_conn = redis.Redis(host="localhost", port=6379, db=0)
+
+# para insert e update
+redis_conn.set("chave1", "trocando_o_meu_valor")
+
+# select
+meu_valor = redis_conn.get("chave1").decode("utf-8")
+
+# delete de dados
+redis_conn.delete("chave1")
+print(meu_valor)
+print(type(meu_valor))
